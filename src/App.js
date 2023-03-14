a2c7ea843e400816bfa4bdaf2f04d19e06ce79a1
sammuel.tuliao <sammuel.tuliao@novare.com.hk>
2023-03-14 17:12:16 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function TodoList() {
+  const [todoItems, setTodoItems] = useState([]);
+  const [isDone, setisDone] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodoItems([...todoItems, { text: inputValue, completed: false }]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodoItems = [...todoItems];
+    newTodoItems.splice(index, 1);
+    setTodoItems(newTodoItems);
+  };
+
+  const handleToggleComplete = (index) => {
+    const newTodoItems = [...todoItems];
+    newTodoItems[index].completed = !newTodoItems[index].completed;
+    setTodoItems(newTodoItems);
+  };
+
+  const handleToggleisDone = () => {
+    setisDone(!isDone);
+  };
+
+  const filterItems = isDone
+    ? todoItems.filter((item) => !item.completed)
+    : todoItems;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        placeholder="Enter a todo item..."
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <button onClick={handleToggleisDone}>
+        {isDone ? "Show Finished" : "Hide Finished"}
+      </button>
+      <ul>
+        {filterItems.map((item, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => handleToggleComplete(index)}
+            />
+            
+              {item.text}
+         
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
